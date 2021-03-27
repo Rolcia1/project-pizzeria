0f26cff3df909b88153e9d00592723ef963debbc
Karolina Zioło <karolina.ziolo@gmail.com>
2021-03-27 10:08:57 +0100

    Add AJAX API

--- src/js/script.js
+++ src/js/script.js
@@ -75,6 +75,12 @@
     cart: {
       defaultDeliveryFee: 20,
     },
+
+    db: {
+      url: '//localhost:3131',
+      product: 'product',
+      order: 'order',
+    },
     // CODE ADDED END
   };
 
@@ -380,6 +386,7 @@
       thisCart.dom.subTotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice);
       thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelector(select.cart.totalPrice);
       thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber);
+      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
 
     }
      
@@ -398,6 +405,41 @@
         thisCart.remove(event.detail.cartProduct);
       });
 
+      thisCart.dom.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisCart.sendOrder();
+      });
+
+    }
+
+
+    sendOrder(){
+      const thisCart = this;
+      const url = settings.db.url + '/' + settings.db.order;
+
+      const payload = {};
+      payload.phone = thisCart.dom.phone.value;
+      payload.address = thisCart.dom.address.value;
+      payload.totalPrice = thisCart.totalPrice;
+      payload.subTotalPrice = thisCart.subTotalPrice;
+      payload.totalNumber = thisCart.totalNumber;
+      payload.deliveryFee = thisCart.deliveryFee;
+      payload.products = [];
+      
+      for(let prod of thisCart.products) {
+        payload.products.push(prod.getData());
+      }
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload)
+      };
+        
+      fetch(url, options);
+      
     }
 
 
@@ -511,9 +553,24 @@
       });
     }
 
+    getData(){
+      const thisCartProduct = this;
+
+      const prod = {};
+      prod.id = thisCartProduct.id;
+      prod.name = thisCartProduct.name;
+      prod.amount = thisCartProduct.amount;
+      prod.priceSingle = thisCartProduct.priceSingle;
+      prod.price = thisCartProduct.price;
+      prod.param = thisCartProduct.param;
+
+      
+      return prod;
+      
+    }
+
+
   }
-
-
 
   const app = {
     initMenu: function() {
@@ -522,14 +579,29 @@
       console.log('thisApp.data:', thisApp.data);
 
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function() {
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.product;
+
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();      
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse: ', parsedResponse);
+          
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+          /* execute initMenu method */
+          thisApp.initMenu();
+        });
+
     },
 
     initCart: function() {
@@ -548,7 +620,7 @@
       console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu();
+     
     
 
     },
